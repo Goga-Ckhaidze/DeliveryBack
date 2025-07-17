9bae073c3b44a24b2bc055df1a1d2ea0bc9f319a
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-07-17 13:38:38 +0400

    sda

--- models/user.js
+++ models/user.js
@@ -28,17 +28,15 @@ userSchema.methods.generateAuthToken = function () {
 const User = mongoose.model('User', userSchema);
 
 const validate = (data) => {
-    const schema = Joi.object({
-        firstName: Joi.string().required().label("First Name"),
-        lastName: Joi.string().required().label("Last Name"),
-        email: Joi.string().email().required().label("Email"),
-        password: passwordComplexity().required().label("Password"),
-        role: Joi.string()
-            .valid('customer', 'delivery')
-            .required()
-            .label("Role")
-    });
-    return schema.validate(data);
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+    role: Joi.string().valid('customer', 'delivery').required().label("Role")
+  });
+  return schema.validate(data, { allowUnknown: true }); // ✅ Allow extra fields like captchaToken
 };
+
 
 module.exports = { User, validate };
