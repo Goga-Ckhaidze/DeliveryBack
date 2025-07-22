c39e91fe0f11e94f33ead7fc075c4908277af938
Goga-Chkhaidze <chxaidze.goga@list.ru>
2025-07-22 11:56:11 +0400

    dsa

--- server.js
+++ server.js
@@ -12,8 +12,17 @@ connection();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.options('*', cors(corsOptions));  // enable pre-flight across-the-board
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://delivery-x5b9.vercel.app', // your frontend URL
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  // allow cookies and auth headers
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/users', userRouters);
